110659bbda9bc7f011bedc5c33fd57f1373bd17a
matheus <contato@matheusgimenez.com.br>
2014-11-28 16:23:47 -0200

    issue #8

--- archive-laboratorio.php
+++ archive-laboratorio.php
@@ -11,18 +11,18 @@ get_header('laboratorio'); ?>
 <div class="col-md-4 pull-left lab_content">
 	<div class="col-md-12" style="margin-top:10px;float:none;clear:both"></div>
 	<h1>Sobre o Imagicas</h1>
-	<p class="intro"><?php echo $opts['lab_intro']; ?></p><!-- .intro -->
+	<p class="intro"><?php echo strip_tags($opts['lab_intro'], '<a>'); ?></p><!-- .intro -->
+	<div class="col-md-12 lab_content_simples">
+		<?php query_posts('lab_tax=simples&posts_per_page=-1'); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+		    <a href="<?php echo get_post_meta( get_the_ID(), 'link_lab', true ); ?>" class="col-md-12 box_simples" target="_blank">
+		    	<span class="pull-left titulo"><?php the_title(); ?></span>
+		    	<span class="pull-right acesse"> >></span>
+		    </a><!-- .col-md-12 box_simples -->
+	    <?php endwhile; ?>
+    </div><!-- .col-md-4 lab_content_simples -->
 </div><!-- .col-md-4 pull-left lab_content -->
-<div class="col-md-3 col-md-offset-1 lab_content_simples">
-	<?php query_posts('lab_tax=simples&posts_per_page=7'); ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-	<a href="<?php echo get_post_meta( get_the_ID(), 'link_lab', true ); ?>" class="col-md-12 box_simples" target="_blank">
-		<span class="pull-left titulo"><?php the_title(); ?></span>
-		<span class="pull-right acesse"> >></span>
-	</a><!-- .col-md-12 box_simples -->
-	<?php endwhile; ?>
-</div><!-- .col-md-4 lab_content_simples -->
-<div class="col-md-4 pull-right lab_content_destaque">
+<div class="col-md-7 pull-right lab_content_destaque">
 	<?php query_posts('lab_tax=destacado&posts_per_page=-1'); ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 	<div class="col-md-6 col-xs-6 destaque_container">
